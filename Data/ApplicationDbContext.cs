9d1a862d64f66567720d98973f52234c6a9ed947
bhandwalkaraniket <137477701+bhandwalkaraniket@users.noreply.github.com>
2024-03-10 18:01:36 +0530

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentPortalApp.Models.Entities;
+
+namespace StudentPortalApp.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+
+
+    }
+    
+}
